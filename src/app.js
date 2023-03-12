df537a1714646bc0e80eb49f799af80c4778bfb5
AnastasiaPanysh <nastya.panysh@gmail.com>
2023-03-12 22:46:41 +0300

    add

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const products = require('./controller/products.controller')
+
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
+app.use(bodyParser.json());
+
+app.use('/products', products)
+
+app.use((error, req, res, next) => {
+    res.status(500).send(error.message)
+})
+
+module.exports = app
