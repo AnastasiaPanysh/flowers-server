f935c433870fe50db12380c4d1b66e3df2ec0710
AnastasiaPanysh <nastya.panysh@gmail.com>
2023-03-15 15:23:17 +0300

    fix route product

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-app.use('/products', products)
+app.use('/product', products)
 app.use('/provider', provider)
 app.use('/customer', customer)
 app.use('/sale', sale)
